61dd151c97e4ab05dc33503e72228049f6d1e8cb
M4tthewDE <mattider2@gmail.com>
2021-09-12 15:53:45 +0200

    move TestServer into separate package

--- main_test.go
+++ main_test.go
@@ -1,122 +1,19 @@
 package main_test
 
 import (
-	"bytes"
-	"context"
-	"encoding/json"
-	"log"
-	"net/http"
 	"testing"
-	"time"
 
-	"github.com/gempir/go-twitch-irc/v2"
-	"github.com/m4tthewde/tmihooks/internal/config"
-	"github.com/m4tthewde/tmihooks/internal/structs"
-	"github.com/stretchr/testify/assert"
+	"github.com/m4tthewde/tmihooks/test"
 )
 
-func TestApplication(t *testing.T) {
+func TestRunApplication(t *testing.T) {
 	t.Parallel()
 
-	testServer := TestServer{
-		t: t,
-	}
+	testServer := test.NewTestServer(t)
 
-	go testServer.startTestClient()
-
-	testServer.registerWebhook()
+	go testServer.StartTestClient()
 
 	for {
 		select {}
 	}
-}
-
-type TestServer struct {
-	t       *testing.T
-	webhook *structs.Webhook
-}
-
-func (ts *TestServer) startTestClient() {
-	http.HandleFunc("/register", ts.register)
-	http.HandleFunc("/chat", ts.chat)
-
-	err := http.ListenAndServe(":7070", nil)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (ts *TestServer) register(w http.ResponseWriter, req *http.Request) {
-	var confirmation structs.Confirmation
-
-	err := json.NewDecoder(req.Body).Decode(&confirmation)
-	if err != nil {
-		panic(err)
-	}
-
-	log.Println("received confirmation")
-
-	s, _ := json.MarshalIndent(confirmation, "", " ")
-
-	log.Println(string(s))
-
-	assert.Equal(ts.t, ts.webhook.Nonce, confirmation.Nonce)
-
-	_, err = w.Write([]byte(confirmation.Challenge))
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (ts *TestServer) chat(w http.ResponseWriter, req *http.Request) {
-	var msg twitch.PrivateMessage
-
-	err := json.NewDecoder(req.Body).Decode(&msg)
-	if err != nil {
-		panic(err)
-	}
-
-	log.Println(msg.Channel, msg.Message)
-}
-
-func (ts *TestServer) registerWebhook() {
-	config := config.GetConfig("test_config.yml")
-	webhook := structs.Webhook{
-		Channels:    []string{"gtawiseguy"},
-		URI:         "http://localhost:7070/chat",
-		RegisterURI: "http://localhost:7070/register",
-		Nonce:       "penis123",
-	}
-	s, _ := json.MarshalIndent(webhook, "", " ")
-
-	log.Println("registering webhook...")
-	log.Println(string(s))
-
-	ts.webhook = &webhook
-
-	webhookJSON, err := json.Marshal(webhook)
-	if err != nil {
-		panic(err)
-	}
-
-	client := &http.Client{}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	address := "http://localhost:" + config.Server.Port + "/register"
-
-	req, err := http.NewRequestWithContext(ctx, "POST", address, bytes.NewBuffer(webhookJSON))
-	if err != nil {
-		log.Println(err)
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println(err)
-	}
-
-	defer resp.Body.Close()
-
-	assert.Equal(ts.t, http.StatusOK, resp.StatusCode)
 }
---
+++ test/server.go
@@ -0,0 +1,115 @@
+package test
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/gempir/go-twitch-irc/v2"
+	"github.com/m4tthewde/tmihooks/internal/config"
+	"github.com/m4tthewde/tmihooks/internal/structs"
+	"github.com/stretchr/testify/assert"
+)
+
+type TestServer struct {
+	t       *testing.T
+	webhook *structs.Webhook
+}
+
+func NewTestServer(t *testing.T) *TestServer {
+	ts := TestServer{
+		t:       t,
+		webhook: nil,
+	}
+
+	return &ts
+}
+
+func (ts *TestServer) StartTestClient() {
+	http.HandleFunc("/register", ts.Register)
+	http.HandleFunc("/chat", ts.chat)
+
+	err := http.ListenAndServe(":7070", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (ts *TestServer) Register(w http.ResponseWriter, req *http.Request) {
+	var confirmation structs.Confirmation
+
+	err := json.NewDecoder(req.Body).Decode(&confirmation)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("received confirmation")
+
+	s, _ := json.MarshalIndent(confirmation, "", " ")
+
+	log.Println(string(s))
+
+	assert.Equal(ts.t, ts.webhook.Nonce, confirmation.Nonce)
+
+	_, err = w.Write([]byte(confirmation.Challenge))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (ts *TestServer) chat(w http.ResponseWriter, req *http.Request) {
+	var msg twitch.PrivateMessage
+
+	err := json.NewDecoder(req.Body).Decode(&msg)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(msg.Channel, msg.Message)
+}
+
+func (ts *TestServer) RegisterWebhook() {
+	config := config.GetConfig("test_config.yml")
+	webhook := structs.Webhook{
+		Channels:    []string{"gtawiseguy"},
+		URI:         "http://localhost:7070/chat",
+		RegisterURI: "http://localhost:7070/register",
+		Nonce:       "penis123",
+	}
+	s, _ := json.MarshalIndent(webhook, "", " ")
+
+	log.Println("registering webhook...")
+	log.Println(string(s))
+
+	ts.webhook = &webhook
+
+	webhookJSON, err := json.Marshal(webhook)
+	if err != nil {
+		panic(err)
+	}
+
+	client := &http.Client{}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	address := "http://localhost:" + config.Server.Port + "/register"
+
+	req, err := http.NewRequestWithContext(ctx, "POST", address, bytes.NewBuffer(webhookJSON))
+	if err != nil {
+		log.Println(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer resp.Body.Close()
+
+	assert.Equal(ts.t, http.StatusOK, resp.StatusCode)
+}
