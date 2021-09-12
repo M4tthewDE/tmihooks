3d4aa13fd1e6e28fb51552e801f845e29d8c3e66
M4tthewDE <mattider2@gmail.com>
2021-09-12 16:32:32 +0200

    add clear function to database handler
    used to clear mongodb before tests run to have clean env

--- internal/db/database.go
+++ internal/db/database.go
@@ -237,3 +237,33 @@ func (dbHandler *DatabaseHandler) GetAllWebhooks() ([]*structs.Webhook, error) {
 
 	return webhooks, nil
 }
+
+func (dbHandler *DatabaseHandler) Clear() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	credential := options.Credential{
+		Username: dbHandler.Config.Database.Password,
+		Password: dbHandler.Config.Database.Password,
+	}
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017").SetAuth(credential))
+	if err != nil {
+		return err
+	}
+
+	collection := client.Database("dev").Collection("webhooks")
+
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err = collection.DeleteMany(
+		ctx,
+		bson.D{},
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
